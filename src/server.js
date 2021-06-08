f42f0ef29f31d1e5a0475b36598e9066d54de7c5
dogi <4leaf.edu.hdk@gmail.com>
2021-06-08 23:07:33 +0900

    server_1

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import helmet from "helmet"
+import morgan from "morgan"
+import path from "path"
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "pug");
+app.use(helmet());
+app.use(morgan(`dev`));
+app.use(express.static(path.join(__dirname,"/assets")));
+
+app.get("/", (req,res) => {
+    res.render("main")
+});
+
+app.listen(PORT, () => {
+    console.log(`${PORT} server start!`);
+})
